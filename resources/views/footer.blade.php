dca534adaad2ef4dfb470f56e343eb07690b03b8
Andrianambinina Fenitra <andrianambininafenitra@gmail.com>
2023-07-29 10:49:46 +0300

    insert dynamic footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,131 @@
+<!-- Start Footer -->
+<footer class="bg-secondary pt-4">
+    <div class="container">
+        <div class="row py-3">
+
+            <div class="col-lg-3 col-12 align-left">
+                <a class="navbar-brand" href="index.html">
+                    <span class="text-light h5">Stack -</span> <span class="text-light h5 semi-bold-600">X</span>
+                </a>
+                <p class="text-light my-lg-4 my-2">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                    sed do eiusmod tempor incididunt ut.
+                </p>
+                <ul class="list-inline footer-icons light-300">
+                    <li class="list-inline-item m-0">
+                        <a class="text-light" target="_blank" href="https://facebook.com/">
+                            <i class='bx bxl-facebook-square bx-md'></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item m-0">
+                        <a class="text-light" target="_blank" href="https://www.linkedin.com/">
+                            <i class='bx bxl-linkedin-square bx-md'></i>
+                        </a>
+                    </li>
+                    <li class="list-inline-item m-0">
+                        <a class="text-light" target="_blank" href="https://www.whatsapp.com/">
+                            <i class='bx bxl-whatsapp-square bx-md'></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-lg-3 col-md-4 my-sm-0 mt-4">
+                <h2 class="h4 pb-lg-3 text-light light-300">Services</h2>
+                <ul class="list-unstyled text-light light-300" style="font-size: small;">
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light" href="{{ route('consultant') }}">Consultant</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="{{ route('dev_web_app') }}">Developpement web et application</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="{{ route('sig') }}">Données géographiques</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="{{ route('objet_connecte') }}">Objets connectés</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="{{ route('maintenance_hebergement') }}">Maintenances et hébergements</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2 col-md-4 my-sm-0 mt-4">
+                <h2 class="h4 pb-lg-3 text-light light-300">Techonologies</h2>
+                <ul class="list-unstyled text-light light-300" style="font-size: small;">
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="https://www.java.com/fr/">Java</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="https://symfony.com/">Symfony</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="https://fr.legacy.reactjs.org/">React.js</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="https://dotnet.microsoft.com/en-us/download/dotnet-framework">.Net</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="https://angular.io/">AngularJs</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="https://www.djangoproject.com/">Django</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="https://wordpress.com/">WordPress</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-4 my-sm-0 mt-4">
+                <h2 class="h4 pb-lg-3 text-light light-300">Société</h2>
+                <ul class="list-unstyled text-light light-300" style="font-size: small">
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="{{ route('a_propos') }}">À propos</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="{{ route('equipe') }}">L'équipe</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="{{ route('pourquoi') }}">Pourquoi Stack-X</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="{{ route('partenaire') }}">Partenaires</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-lg-2 col-md-4 my-sm-0 mt-4">
+                <h2 class="h4 pb-lg-3 text-light light-300">Contact</h2>
+                <ul class="list-unstyled text-light light-300" style="font-size: small;">
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="tel:038-472-9745">+261 38 47 297 45</a>
+                    </li>
+                    <li class="pb-2">
+                        <a class="text-decoration-none text-light py-1" href="mailto:stack.xlab@gmail.com">stack.xlab@gmail.com</a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="w-100 bg-primary py-3">
+        <div class="container">
+            <div class="row pt-2">
+                <div class="col-lg-6 col-sm-12">
+                    <p class="text-lg-start text-center text-light light-300">
+                        © Copyright 2023 <strong> Stack-X </strong> . Tout droit réservé.
+                    </p>
+                </div>
+                <div class="col-lg-6 col-sm-12">
+                    <p class="text-lg-end text-center text-light light-300">
+                        <a rel="sponsored" class="text-decoration-none text-light" href="https://templatemo.com/" target="_blank"><strong>ZokyRal</strong></a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+<!-- End Footer -->
